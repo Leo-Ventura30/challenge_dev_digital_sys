fbd6538c39c34870e8f23b19eafb4196cf196d01
Leo Ventura <leonardo.ventura@outlook.com>
2023-06-24 11:59:21 -0300

    fix: trocando chave da tupla de status

--- customers/utils.py
+++ customers/utils.py
@@ -5,21 +5,22 @@ def validate_proposal(value, score):
         value = value / 2
     else:
         return value
-
     return value
 
 
 def compare_score(approved_value, score):
     STATUS_TYPE = (
-        (1,  'Aprovada'),
-        (2,  'Parcialmente Aprovada'),
-        (3, 'Negada'),
+        ('aprovada',  'Aprovada'),
+        ('parcial_aprovada',  'Parcialmente Aprovada'),
+        ('negada', 'Negada'),
+        ('em_analise', 'Em analise'),
     )
-    if (approved_value > 0 and score <= 300):
-        status = STATUS_TYPE[0][1]
+
+    if (approved_value == 0 or score <= 300):
+        status = STATUS_TYPE[2][0]
     elif (score > 300 and score <= 600):
-        status = STATUS_TYPE[1][1]
+        status = STATUS_TYPE[1][0]
     else:
-        status = STATUS_TYPE[2][1]
+        status = STATUS_TYPE[0][0]
 
     return status
